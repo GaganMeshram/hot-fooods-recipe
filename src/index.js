63e408e5741ef32d609f46df73607dabc6a2de74
Gagan <gmeshram774@gmail.com>
2023-01-03 15:50:31 +0530

    modified

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <BrowserRouter  basename={process.env.PUBLIC_URL}>
+    <HashRouter  basename='/'>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </>
 );
