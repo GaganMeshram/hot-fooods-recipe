25ae5586ab0130e8a40e2ea852f7bd9cd5b91ab7
Gagan <gmeshram774@gmail.com>
2023-01-03 14:54:46 +0530

    added basname in browser

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { HashRouter ,BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <HashRouter basename="/">
+    <BrowserRouter  basename={process.env.PUBLIC_URL}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </>
 );
