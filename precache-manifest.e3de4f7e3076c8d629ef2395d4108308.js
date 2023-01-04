de08e7d28458f437f66d46a1a4b6195ad352ddb3
Gagan <gmeshram774@gmail.com>
2023-01-04 15:08:05 +0530

    Updates

---
+++ precache-manifest.e3de4f7e3076c8d629ef2395d4108308.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c66d51e165c968965f4",
+    "url": "/hot-foods-recipe/static/css/main.7dacd013.chunk.css"
+  },
+  {
+    "revision": "2c66d51e165c968965f4",
+    "url": "/hot-foods-recipe/static/js/main.ce551da9.chunk.js"
+  },
+  {
+    "revision": "bc9fbc601702cbbc8969",
+    "url": "/hot-foods-recipe/static/js/runtime~main.8886e8fa.js"
+  },
+  {
+    "revision": "e15333f87e3f7515481a",
+    "url": "/hot-foods-recipe/static/js/2.51c69e26.chunk.js"
+  },
+  {
+    "revision": "b57fcde742a2dd64019fb1f689503be4",
+    "url": "/hot-foods-recipe/static/media/hero-image.b57fcde7.jpg"
+  },
+  {
+    "revision": "18d48d17aabda5e377fc5ea39c7968b4",
+    "url": "/hot-foods-recipe/static/media/american-2.18d48d17.jpg"
+  },
+  {
+    "revision": "bf5e18c585c82660d6556a2a47e7a019",
+    "url": "/hot-foods-recipe/static/media/chinese.bf5e18c5.jpg"
+  },
+  {
+    "revision": "a366ea3ef5a8ec988e9242e46e9ab64f",
+    "url": "/hot-foods-recipe/static/media/indian.a366ea3e.jpg"
+  },
+  {
+    "revision": "9b5b82cef3213625964364ab594ab96c",
+    "url": "/hot-foods-recipe/static/media/italian.9b5b82ce.jpg"
+  },
+  {
+    "revision": "c75cc27b62f3559b6e6dc5a35583aee2",
+    "url": "/hot-foods-recipe/index.html"
+  }
+];
